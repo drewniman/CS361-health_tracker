3742fd064d61aad306fddf4ea899fb8534a80c7c
Drew Niman <drewmniman@gmail.com>
2023-01-26 10:15:37 -0800

    added signup authentication

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -12,4 +14,21 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email is too short', category='error')
+        elif len(firstName) < 2:
+            flash('First name is too short', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match', category='error')
+        elif len(password1) < 7:
+            flash('Password is too short', category='error')
+        else:
+            flash('Sign-up successful!', category='success')
+
     return render_template("sign_up.html")
