5bf125b5998285c664ed539d8bb3c10702aba79f
Drew Niman <drewmniman@gmail.com>
2023-01-26 22:21:28 -0800

    restricted pages to logged in users

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -14,6 +15,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Login successful!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password.', category='error')
@@ -22,8 +24,10 @@ def login():
     return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -48,6 +52,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Sign-up successful!', category='success')
             return redirect(url_for('views.home'))
 
