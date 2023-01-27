bfba7f2b8e2eae0377bd572622ec1b3de61601bb
Drew Niman <drewmniman@gmail.com>
2023-01-26 22:28:28 -0800

    restricted navbar based on login status

--- website/auth.py
+++ website/auth.py
@@ -21,7 +21,7 @@ def login():
                 flash('Incorrect password.', category='error')
         else:
             flash('Email not associated with an account.', category='error')
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -56,4 +56,4 @@ def sign_up():
             flash('Sign-up successful!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
