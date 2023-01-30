594c117c509772bcb7a845e8ebbf67352374564a
Drew Niman <drewmniman@gmail.com>
2023-01-30 10:20:06 -0800

    can update health metrics

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import Snapshot
 from flask_login import login_required, current_user
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -21,4 +23,26 @@ def nutrition():
 @views.route('/health_snapshot', methods=['GET', 'POST'])
 @login_required
 def health_snapshot():
+    if request.method == 'POST':
+        height = request.form.get('height')
+        weight = request.form.get('weight')
+        blood_pressure = request.form.get('blood_pressure')
+        waist = request.form.get('waist')
+        bf_percent = request.form.get('bf_percent')
+
+        if not (36 < int(height) < 100):
+            flash('Height out of range', category='error')
+        elif not (80 < int(weight) < 800):
+            flash('Weight out of range', category='error')
+        elif not (80 < int(blood_pressure) < 220):
+            flash('Blood pressure out of range', category='error')
+        elif not (10 < int(waist) < 100):
+            flash('Waist circumference out of range', category='error')
+        elif not (3 < int(bf_percent) < 90):
+            flash('Body fat percentage out of range', category='error')
+        else:
+            new_snapshot = Snapshot(height=height, weight=weight, blood_pressure=blood_pressure, waist=waist, bf_percent=bf_percent, user_id=current_user.id)
+            db.session.add(new_snapshot)
+            db.session.commit()
+            flash('Health metrics updated!', category='success')
     return render_template("health_snapshot.html", user=current_user)
